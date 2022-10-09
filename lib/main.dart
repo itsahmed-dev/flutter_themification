96fbe5db677ab31042faa5e9d3725aeb0dfaed5a
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-10-09 18:12:03 +0500

    First Commit in Flutter Themification Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const StarterApp());
+void main() => runApp(const ThemificationApp());
 
-class StarterApp extends StatelessWidget {
-  const StarterApp({super.key});
+class ThemificationApp extends StatelessWidget {
+  const ThemificationApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Starter App',
+      title: 'Themification',
       home: HomePage(),
     );
   }
