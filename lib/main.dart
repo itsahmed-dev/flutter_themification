0b0c6162bc1e9bddecd6edb3124bb3b0590c8e82
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-10-11 14:37:36 +0500

    All text types in textTheme

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class ThemificationApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Themification',
-      theme: theme,
+      theme: customTheme,
       // darkTheme: darkTheme,
       home: HomePage(),
     );
@@ -23,6 +23,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
     return Scaffold(
       appBar: AppBar(
         title: Text('Themification'),
@@ -30,37 +31,53 @@ class HomePage extends StatelessWidget {
       ),
       drawer: Drawer(),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text('Text'),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Elevated Button'),
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text('Display Large', style: textTheme.displayLarge),
+              Text('Display Medium', style: textTheme.displayMedium),
+              Text('Display Small', style: textTheme.displaySmall),
+              Text('Headline Large', style: textTheme.headlineLarge),
+              Text('Headline Medium', style: textTheme.headlineMedium),
+              Text('Headline Small', style: textTheme.headlineSmall),
+              Text('Title Large', style: textTheme.titleLarge),
+              Text('Title Medium', style: textTheme.titleMedium),
+              Text('Title Small', style: textTheme.titleSmall),
+              Text('Label Large', style: textTheme.labelLarge),
+              Text('Label Medium', style: textTheme.labelMedium),
+              Text('Label Small', style: textTheme.labelSmall),
+              Text('Body Large', style: textTheme.bodyLarge),
+              Text('Body Medium', style: textTheme.bodyMedium),
+              Text('Body Small', style: textTheme.bodySmall),
+              // ElevatedButton(
+              //   onPressed: () {},
+              //   child: Text('Elevated Button'),
+              // ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ),
-        ],
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {},
+      //   child: Icon(Icons.add),
+      // ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   items: const <BottomNavigationBarItem>[
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.home),
+      //       label: 'Home',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.business),
+      //       label: 'Business',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.school),
+      //       label: 'School',
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
--- lib/themes.dart
+++ lib/themes.dart
@@ -1,6 +1,36 @@
 import 'package:flutter/material.dart';
 
-ThemeData theme = ThemeData();
+ThemeData customTheme = ThemeData(
+  textTheme: TextTheme(
+    displayLarge: TextStyle(color: Colors.black),
+    displayMedium: TextStyle(
+      color: Colors.black,
+      backgroundColor: Colors.yellow,
+    ),
+    displaySmall: TextStyle(
+      color: Colors.black,
+      decoration: TextDecoration.lineThrough,
+    ),
+    headlineLarge: TextStyle(
+      color: Colors.black,
+      decoration: TextDecoration.overline,
+    ),
+    headlineMedium: TextStyle(
+      color: Colors.black,
+      decoration: TextDecoration.underline,
+    ),
+    headlineSmall: TextStyle(color: Colors.black),
+    titleLarge: TextStyle(color: Colors.black),
+    titleMedium: TextStyle(color: Colors.black),
+    titleSmall: TextStyle(color: Colors.black),
+    labelLarge: TextStyle(color: Colors.black),
+    labelMedium: TextStyle(color: Colors.black),
+    labelSmall: TextStyle(color: Colors.black),
+    bodyLarge: TextStyle(color: Colors.black),
+    bodyMedium: TextStyle(color: Colors.black),
+    bodySmall: TextStyle(color: Colors.black),
+  ),
+);
 
 ThemeData lightTheme = ThemeData.light();
 
